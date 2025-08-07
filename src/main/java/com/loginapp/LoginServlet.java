7c5a703a7a889b4d1b085c6212d597d7fd837069
rohanrode02 <roderohan2@gmail.com>
2025-08-07 16:25:19 +0530

    maven project practice

---
+++ src/main/java/com/loginapp/LoginServlet.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.sql.*;
+
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String user = request.getParameter("username");
+        String pass = request.getParameter("password");
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/studentdb", "root", "root");
+
+            PreparedStatement ps = con.prepareStatement(
+                "SELECT * FROM users WHERE username=? AND password=?");
+            ps.setString(1, user);
+            ps.setString(2, pass);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                response.sendRedirect("success.jsp");
+            } else {
+                response.getWriter().println("Login Failed. Try Again.");
+            }
+
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ target/java-login-db-demo/success.jsp
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head><title>Success</title></head>
+<body>
+    <h2>Login Successful!</h2>
+    <h2>congartulatin your maven project is deploy on tomcat servers</h2>
+</body>
+</html>
